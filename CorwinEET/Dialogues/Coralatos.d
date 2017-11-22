e38c58707288cbf970c4c95899b43870f379dbd1
RoxanneSHS <cwillenbockel@aol.com>
2017-11-22 17:28:09 +0100

    Work in Progress

---
+++ CorwinEET/Dialogues/Coralatos.d
@@ -0,0 +1,16 @@
+CHAIN
+IF WEIGHT #-5~InParty("Corwin")~THEN Alatos Alert
+~Beware! Am I surrounded by nothing but fools! A Flaming Fist officer walks into here just like that.~
+==BDCorwiJ~Oh, Thuibuld, stop the charade. I'm not the first nor the last of the Fist to be welcome here.~
+==Alatos~Ah, you're one on my payrole, lady, I should've known. I just can't know all of you personally, so sorry.~
+END
+++~Corwin, nice attempt, but...~+ Alert2
+++~Your messenger outside made it somehow urgent for us to see you.~EXTERN Alatos 0
+
+CHAIN
+IF~~THEN Alatos Alert2
+~Corwin? Captain Schael Corwin!!~
+=~How many of my men are in jail or worse thanks to you. The unerring Corwin is surely not in our services.~
+==BDCorwiJ~<CHARNAME>, you're an idiot. It's wonder you made it this far.~
+END
+++~Wait...~EXTERN Alatos 3
