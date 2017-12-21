1e756415e9204b329895551552f379570cc6fdcf
RoxanneSHS <cwillenbockel@aol.com>
2017-12-21 23:09:11 +0100

    Work in Progress

--- CorwinEET/Dialogues/Corscar.d
+++ CorwinEET/Dialogues/Corscar.d
@@ -90,3 +90,7 @@ I_C_T Laola 0 EELaolaCorw
 ==BDCorwiJ IF~InParty("Corwin")~THEN~Laola, they already agreed to cooperate with Scar and me as you can see.~
 ==Laola IF~InParty("Corwin")~THEN~Oh, Schael, excuse me, Captain Corwin, I didn't see you at first.~
 ==Laola IF~InParty("Corwin")~THEN~Yes, Commander Scar told me about your plan to seek <CHARNAME>'s company but wasn't sure if they'd accept the offer. I'm glad they did.~END
+
+INTERJECT_COPY_TRANS IF_FILE_EXISTS BW05FFGE 2 EECorGreyClan
+==BDCorwiJ IF~InParty("Corwin")~THEN~General, I accompany <CHARNAME> with Scar's and Eltan's consent to help out with my knowledge best I can.~
+==IF_FILE_EXISTS BW05FFGE IF~InParty("Corwin")~THEN~Ah, Captain Corwin, greetings. Your presence with <PRO_HIMHER> assures me that I adress the right people.~END
