190cf621536ecc519741ca5e7dd4ef0945031ee7
RoxanneSHS <cwillenbockel@aol.com>
2017-12-28 13:46:58 +0100

    Work in Progress

--- CorwinEET/Dialogues/CorEncounters.d
+++ CorwinEET/Dialogues/CorEncounters.d
@@ -14,3 +14,18 @@ I_C_T Laola 0 EELaolaCorw
 ==Laola IF~InParty("Corwin")~THEN~Oh, Schael, excuse me, Captain Corwin, I didn't see you at first.~
 ==Laola IF~InParty("Corwin")~THEN~Yes, Commander Scar told me about your plan to seek <CHARNAME>'s company but wasn't sure if they'd accept the offer. I'm glad they did.~END
 
+CHAIN
+IF WEIGHT #-3 ~NumberOfTimesTalkedTo(0) InParty("Corwin")~THEN Housg3 EECorwinInEntar1
+~You there! What are you doing in the estate of Entar Silvershield?~
+== BDCorwiJ~I'm Captain Corwin of the Flaming Fist. We're here to ensure the Duke's safety.~
+END
+++~We're here because we have dealings to discuss with Entar Silvershield. My associates and I have come all the way from Waterdeep. Perhaps you could leave us to our business now? ~+ 5
+++~We're members of the Flaming Fist. There has been some worry about the security of Entar's estate.~+ 5
+IF~OR(2)Global("X#KagainCaravan","GLOBAL",5)PartyHasItem("X#SILSH")~THEN REPLY~We bring news for Lord and Lady Silvershield. We believe that we have found news of their son. Any chance this pin is one of yours?~+ 7
+
+
+INTERJECT Skie 1 EECorwinSkieBG1
+==BDCorwiJ IF~InParty("Corwin") !InParty("Eldoth")~THEN~Hello, Skie...~
+==Skie IF~InParty("Corwin") !InParty("Eldoth")~THEN~Oh, Schael, my father never lets me out much. If I could just join the Flaming Fist, I could be free just like you.~
+==BDCorwiJ IF~InParty("Corwin") !InParty("Eldoth")~THEN~Now...apart from the fact that your father probably never woould agree to it, there's something like discipline very high up in the Fist's rules, while free is...well, it's a dream anyway.~
+==Skie IF~InParty("Corwin") !InParty("Eldoth")~THEN~Ah, you just wait and see, I tell you, I'll grab my chance if I see it.~DO~EscapeAreaDestroy(30)~EXIT
