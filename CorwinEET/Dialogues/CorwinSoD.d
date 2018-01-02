ba1670304f6ef79af4a65f4cf5416afc7305b04a
RoxanneSHS <cwillenbockel@aol.com>
2018-01-02 22:27:39 +0100

    v0.2b

--- CorwinEET/Dialogues/CorwinSoD.d
+++ CorwinEET/Dialogues/CorwinSoD.d
@@ -58,3 +58,66 @@ CHAIN
 IF WEIGHT #-7~Global("bd_plot","global",591) GlobalGT("EERescEltan","Locals",1)~THEN BDCorwin Skiefound
 ~You risked your life for Duke Eltan when he was dying...your feelings about Lanie you shared with me...all of that IS TRUE - this here isn't. Tell me it isn't, I will believe you.~EXTERN BDCorwin 52
 
+CHAIN
+IF~Global("EECorwAfter","BD0120",1)~THEN BDCorwiJ Korlasz1
+~I'm a bit concerned that Sarevok really proclaimed himself to be your brother.~
+DO~SetGlobal("EECorwAfter","BD0120",2)~
+=~I don't believe that two spawns of a dead god are siblings in any way.~
+END
+++~Then why are you concerned?~+ Korlasz2
+++~Neither do I - and even if we are, it does not impact me.~+ Korlasz2
+++~I doesn't matter if he really were family, my choices would've been just the same.~+ Korlasz2
+
+CHAIN
+IF~~THEN BDCorwiJ Korlasz2
+~What bothers me is that soon the people of Baldur's Gate will blame themself for having trusted the Bhaalspawn Sarevok and they will celebrate you, the winner, the hero, whatnot.~
+=~Public opinion is a fragile thing, <CHARNAME>. I love my hometown but I don't trust a mob that sees Sarevok's <PRO_BROTHERSISTER> in you. It may raise suspicion and rumour pretty soon.~
+END
+IF~ReputationLT(Player1,10)~THEN REPLY~And what do you think, Corwin?~+ Korlasz4
+IF~ReputationGT(Player1,9)~THEN REPLY~And what do you think, Corwin?~+ Korlasz3
+
+CHAIN
+IF~~THEN BDCorwiJ Korlasz3
+~I trust you, my friend.~EXIT
+
+CHAIN
+IF~~THEN BDCorwiJ Korlasz4
+~Time will tell which way you're heading, <CHARNAME>.~EXIT
+
+CHAIN
+IF~Global("EECorwAfter","BD0120",3)~ THEN BDCorwiJ Korlasz11
+~<CHARNAME>, my mission...our mission is finished for the moment. We have done well, I guess. It's time to restore what is left of the Flaming Fist.~
+DO~SetGlobal("EECorwAfter","BD0120",4)~
+END
+IF~Gender(Player1,MALE)~THEN REPLY~Are you trying to tell me that you have to leave me now?~+ Korlasz12m
+IF~Gender(Player1,MALE)~THEN REPLY~We've really earned ourself some rest and maybe some celebration.~+ Korlasz12m
+IF~Gender(Player1,FEMALE)~THEN REPLY~Are you trying to tell me that you have to leave me now?~+ Korlasz12f
+IF~Gender(Player1,FEMALE)~THEN REPLY~We've really earned ourself some rest and maybe some celebration.~+ Korlasz12f
+
+CHAIN
+IF~~THEN BDCorwiJ Korlasz12m
+~It's time for you to take some rest after all this, <CHARNAME>. And then the town wants to celebrate it's new hero.~EXTERN BDCorwiJ Korlasz13
+
+CHAIN
+IF~~THEN BDCorwiJ Korlasz12f
+~It's time for you to take some rest after all this, <CHARNAME>. And then the town wants to celebrate it's new heroine.~EXTERN BDCorwiJ Korlasz13
+
+CHAIN
+IF~~THEN BDCorwiJ Korlasz13
+~It's my duty to see how far Duke Eltan is restored by now. He needs all those officers who stayed loyal to clean the Flaming Fist of Angelo's remains and bring back the order to the city.~
+=~The Fist needs me now, my hometown needs me now...and Rohma needs me as well.~
+END
+++~I hope we will soon meet again, Corwin.~+ Korlasz14
+++~I understand your motivation, Captain, I hope it's a temporary thing though.~+ Korlasz14
+++~Yeah, you served me well, now go and serve your Eltan if you must.~+ Korlasz15
+
+CHAIN
+IF~~THEN BDCorwiJ Korlasz15
+~You make parting easier than I thought it would be. Good luck, <CHARNAME>.~
+DO~DestroyItem("EEAmul") LeaveParty() SetGlobal("EECorwinBG1","Global",1) EscapeAreaDestroy(45)~EXIT
+
+CHAIN
+IF~~THEN BDCorwiJ Korlasz14
+~You don't have pressing business, <CHARNAME>., why not take your time to learn about Baldur's Gate from a nicer perspective.~
+=~I...I look ahead to see you again, my friend.~
+DO~DestroyItem("EEAmul") LeaveParty() SetGlobal("EECorwinBG1","Global",1) EscapeAreaDestroy(45)~EXIT
